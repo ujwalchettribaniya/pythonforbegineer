f0ead42ba3358f4c05fcf8c79d94ae36f06be692
Ujwal Baniya <ujwalchettribaniya@gmail.com>
2026-04-19 22:37:17 +0545

    first commit

--- Chapter 1-PS/module.py
+++ Chapter 1-PS/module.py
@@ -2,6 +2,13 @@ import pyjokes
 
 joke=pyjokes.get_joke()
 print(joke)
+print("joke is not used with parentheses because it is not a function but a variable which stores the joke from the module")
+print("print is a function and syntax is print()")
+print("we use modeles to use the code which is already written by other people and we can use it in our code without writing the code from scratch")
+print("variable is used to store the value example joke is a variable which stores the joke from the module")
+print("string is a sequence of characters and we use double quotes to define a string")
+print("int is a data type which is used to store whole numbers")
+print("7+8 is done in without double quotes because it is an int and it will give the output as 15 but if we write "7+8" it will give the output as 7+8 because it is a string")
 
 print("To install module we use python -m pip install module_name")
 print("For now python -m pip install pyjokes is used")
---
+++ Chapter 2/print.py
@@ -0,0 +1,4 @@
+print("print is used to print with paranthesis and double quotes")
+print("Hello World, 7")
+print("7+8, 7*8, 7-8, 7/8")
+print(7+8, 7*8, 7-8, 7/8)
